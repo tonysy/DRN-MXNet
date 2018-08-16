af2c02f6bb22a25c036700b26248e9b01294eaad
tonysy <sy.zhangbuaa@gmail.com>
2018-08-16 11:13:58 +0800

    add .c for init.sh

---
+++ experiment/deeplab/drn_test.py
@@ -0,0 +1,12 @@
+import os
+import sys
+os.environ['PYTHONUNBUFFERED'] = '1'
+os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
+os.environ['MXNET_ENABLE_GPU_P2P'] = '0'
+this_dir = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(this_dir, '..', '..', 'deeplab'))
+
+import train_v1
+import test_v1
+if __name__ == "__main__":
+    test_v1.main()
